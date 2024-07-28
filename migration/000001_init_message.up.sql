724ebf7d15e00d3c2c7e81e6662def71b3f3eda7
r_angel <143842574+6angel6@users.noreply.github.com>
2024-07-28 14:10:38 +0500

    refactor: sql script

--- migration/000001_init_message.up.sql
+++ migration/000001_init_message.up.sql
@@ -1,6 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE Messages (
-                          id SERIAL PRIMARY KEY,
+                          id UUID DEFAULT uuid_generate_v4(),
                           content TEXT NOT NULL,
                           status VARCHAR(20) DEFAULT 'pending',
-                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          PRIMARY KEY(id)
 )
